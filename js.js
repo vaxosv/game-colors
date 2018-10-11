90023e4d4f43d4ed15266d21933472e67a020149
vakhtang <vaxo.sv@gmail.com>
2018-10-11 15:31:27 +0400

    basic logic and responcive desing

--- js.js
+++ js.js
@@ -1,9 +1,12 @@
 let Cwidth;
-let Cheight = 300;
+let Cheight = 200;
 let fromtop = 0;
 let tuchCount = 0;
-let goOn;
+let goOn = true;
 let wallcolor = 0;
+let score = 0;
+let highScore = 0;
+let speed = 3;
 let colors = [
     [255, 0, 0],
     [0, 255, 0],
@@ -11,49 +14,138 @@ let colors = [
 ]
 
 function setup() {
-    Cwidth = windowWidth / 2 - 40;
+    Cwidth = windowWidth;
     createCanvas(windowWidth, windowHeight);
     background(255, 204, 0);
 }
 
 function draw() {
-    objects()
-    mainAnimation();
+    //     objects()
+    //     mainAnimation();
 
-    logic()
-}
-
-function mousePressed() {
-    goOn = true;
-    tuchCount++
-    if (tuchCount > 2) {
-        tuchCount = 0;
-    }
+    //     logic()
+    game()
 }
 
 function mainAnimation() {
-    fromtop += 3;
+    fromtop += speed;
     if (fromtop >= windowHeight) {
         fromtop = 0;
-        wallcolor = Math.floor( random(0, 2))
+        score++
+        speed += 0.3
+        wallcolor = Math.floor(random(0, 2))
         console.log(wallcolor);
     }
 }
 
 function objects() {
+
+    noStroke()
     background(255, 204, 0);
     fill(colors[wallcolor])
     rect(0, fromtop, Cwidth, Cheight);
-    rect(300, fromtop, Cwidth, Cheight);
 
+    stroke('rgba(34, 34, 34, 0.3)');
+    strokeWeight(2);
     fill(colors[tuchCount])
-    rect(170, 500, 100, 100);
+    rect((windowWidth / 2) - 50, windowHeight - 100, 100, 100);
+    Score()
 }
 
 
 function logic() {
+    if (fromtop + Cheight >= (windowHeight - 100) && tuchCount != wallcolor) {
+        textSize(30);
+        fill(0)
+        text("Game Over", windowWidth / 2 - 80, 100)
+        score = 0;
+        speed = 3;
+        goOn = false
+    } else if (fromtop >= (windowHeight - 100) && tuchCount === wallcolor) {
+        textSize(30);
+        fill(0)
 
-    if (fromtop >= (windowHeight - 300) && tuchCount != wallcolor) {
-        console.log("wavage");
+        text("play music", windowWidth / 2 - 100, 100)
+        goon = true
+    }
+}
+
+function game() {
+    if (goOn) {
+        objects()
+        mainAnimation();
+        logic()
+    } else {
+        background(255, 204, 0);
+        fromtop = 0
+        textSize(30);
+        fill(0)
+        text("Touch To play", windowWidth / 2 - 100, 100)
+        // ScoreSave()
+    }
+
+}
+
+
+function Score() {
+    textSize(20)
+    fill(240)
+    noStroke()
+    text(score, 20, 30)
+}
+
+function ScoreSave() {
+    if (score > highScore) {
+        highScore = score;
+    }
+    highScore = toString(highScore)
+    localStorage.setItem('highScore', highScore);
+    var localscore = localStorage.getItem('highScore');
+    console.log(localscore)
+}
+
+
+
+
+
+
+
+var rate = 100;
+var lastClick = Date.now() - rate;
+var button = document.querySelector('html');
+button.addEventListener('touchstart', () => {
+    if (Date.now() - lastClick >= rate) {
+        goOn = true;
+        tuchCount++
+        if (tuchCount > 2) {
+            tuchCount = 0;
+        }
+        lastClick = Date.now();
+    }
+});
+
+// var rate = 100;
+// var lastClick = Date.now() - rate;
+// var button = document.querySelector('html');
+// button.addEventListener('click', () => {
+//   if (Date.now() - lastClick >= rate) {
+//      goOn = true;
+//     tuchCount++
+//     if (tuchCount > 2) {
+//         tuchCount = 0;
+//     }
+//     lastClick = Date.now();
+//   }
+// });
+
+
+class Wall {
+    constructor() {
+
+    }
+
+    draw() {
+        fill(colors[wallcolor])
+        rect(0, fromtop, Cwidth, Cheight);
     }
 }
